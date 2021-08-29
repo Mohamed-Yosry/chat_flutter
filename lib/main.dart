19f53e1918dc9254ff29b3192a62a6a6461dc44b
Mohamed <mohmedyosry13@gmail.com>
2021-08-30 01:08:43 +0200

    fixed gradle error

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:chat_flutter/registration/RegistrationScrenn.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:chat_flutter/creatingGroup/createGroupPage.dart';
 import 'package:provider/provider.dart';
 
 import 'AppProvider.dart';
