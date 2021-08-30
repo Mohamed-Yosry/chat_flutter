5489ab35cd8d2b60a73b0d18996342e3316ab7a4
Mohamed M. Mansour <mohamed_mahmoud96357@yahoo.com>
2021-08-30 21:52:21 +0200

    added tab bar and the implementation for displaying the rooms

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -35,70 +35,150 @@ class _HomeScreenState extends State<HomeScreen> {
             height: double.infinity,
           ),
         ),
-        Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: _isSeacrhIconPressed? AppBar(
-            automaticallyImplyLeading: false,
-            elevation: 0,
+        DefaultTabController(
+          length: 2,
+          child: Scaffold(
             backgroundColor: Colors.transparent,
-            actions: [
-              Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 12, bottom: 12, left: 25, right: 7),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(35),
-                      ),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: "Search Room Name",
-                          hintStyle: TextStyle(color: Color(0x593598DB), fontSize: 14),
-                          prefixIcon: IconButton(
-                            onPressed: (){
+            appBar: _isSeacrhIconPressed? AppBar(
+              automaticallyImplyLeading: false,
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              actions: [
+                Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 12, bottom: 12, left: 25, right: 7),
+                      child: Container(
+                        height: 48,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(35),
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Search Room Name",
+                            hintStyle: TextStyle(color: Color(0x593598DB), fontSize: 14),
+                            prefixIcon: IconButton(
+                              onPressed: (){
 
-                            },
-                            color: Color(0xFF3598DB),
-                            icon: Icon(Icons.search),
+                              },
+                              color: Color(0xFF3598DB),
+                              icon: Icon(Icons.search),
+                            ),
+                            border: InputBorder.none,
                           ),
-                          border: InputBorder.none,
                         ),
                       ),
-                    ),
-                  )
-              )
-            ],
-          )
-          :AppBar(
-            elevation: 0,
-            backgroundColor: Colors.transparent,
-            title: new Text(
-                "Chat App",
-                style: TextStyle(color: Colors.white, fontSize: 21, fontWeight: FontWeight.bold)
-            ),
-            centerTitle: true,
-            actions: [
-              IconButton(
-                onPressed: (){
-                  setState(() {
-                    _isSeacrhIconPressed=true;
-                  });
-                },
-                icon: Icon(Icons.search),
-              )
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: (){
-              Navigator.pushNamed(context, CreateGroup.routeName);
-            },
-            child: Icon(Icons.add),
-          ),
-          drawer: Drawer(),
-          body : Container(
+                    )
+                )
+              ],
+            )
+            :AppBar(
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              title: new Text(
+                  "Chat App",
+                  style: TextStyle(color: Colors.white, fontSize: 21, fontWeight: FontWeight.bold)
+              ),
+              centerTitle: true,
+              actions: [
+                IconButton(
+                  onPressed: (){
+                    setState(() {
+                      _isSeacrhIconPressed=true;
+                    });
+                  },
+                  icon: Icon(Icons.search),
+                )
+              ],
 
-          )
-        ),
+              bottom: TabBar(
+                indicatorColor: Colors.white,
+                tabs: [
+                  Tab(
+                    child: Text(
+                      "Browse",
+                      style: TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      "My Rooms",
+                      style: TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: (){
+                Navigator.pushNamed(context, CreateGroup.routeName);
+              },
+              child: Icon(Icons.add),
+            ),
+            drawer: Drawer(),
+            body:
+                TabBarView(
+                  children: [
+                    Container(
+                       child: FutureBuilder<QuerySnapshot<Room>>(
+                          future: roomCollection.get(),
+                          builder:
+                              (BuildContext context, AsyncSnapshot<QuerySnapshot<Room>> snapshot) {
+
+                            if (snapshot.hasError) {
+                              return Text("Something went wrong");
+                            }
+
+                            if (snapshot.connectionState == ConnectionState.done) {
+                              final List<Room> myRoomList = snapshot.data?.docs.map((e) => e.data()).toList() ??[];
+                              return myRoomList.length == 0 ?  Center(child: Text("No rooms available")):
+                               Expanded(
+                                 child: GridView.builder(
+                                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                      crossAxisCount: 2,
+                                      crossAxisSpacing: 21,
+                                      mainAxisSpacing: 21,
+                                      childAspectRatio: 0.88,
+                                    ),
+                                  itemBuilder: (buildContex, index){
+                                      return InkWell(
+                                        onTap: (){
+                                          /// go to description and join room page
+                                          /// Navigator.pushNamed(context, JoinRoom.routeName);
+                                        },
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            color: Colors.white,
+                                            borderRadius: BorderRadius.circular(12),
+                                          ),
+                                          child: Column(
+                                            children: [
+                                              Image(
+                                                  image: AssetImage('assets/categories/${myRoomList[index].category}.png'),
+
+                                              ),
+                                              Text(myRoomList[index].name),
+                                            ],
+                                          ),
+                                        ),
+                                      );
+                                  },itemCount: myRoomList.length,
+                              ),
+                               );
+                            }
+
+                            return Center(child :CircularProgressIndicator());
+                          },
+                        )
+                    ),
+                    Container(),
+                  ],
+                ),
+
+
+
+            ),
+        )
       ],
     );
   }
