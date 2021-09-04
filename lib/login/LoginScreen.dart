58da6824b7420583e20df1f970f41ebc9a2cbbe4
Mohamed M. Mansour <mohamed_mahmoud96357@yahoo.com>
2021-09-04 22:22:26 +0200

    now we can cancel the search

--- lib/login/LoginScreen.dart
+++ lib/login/LoginScreen.dart
@@ -5,7 +5,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
-import 'package:chat_flutter/User.dart' as MyUser;
 
 import '../AppProvider.dart';
 import '../HomeScreen.dart';
